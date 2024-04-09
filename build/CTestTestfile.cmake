5491c779dda2ffda6c1176baab8046a06543b4e0
Sergey Shchuka <schuka.s01@gmail.com>
2024-04-09 11:50:56 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/PROJECT/zlib-1.3.1
+# Build directory: D:/PROJECT/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "D:/PROJECT/zlib-1.3.1/CMakeLists.txt;203;add_test;D:/PROJECT/zlib-1.3.1/CMakeLists.txt;0;")
